0e1f9a58ae3304b354a5e79966d6c6cb34ae0f31
Shahid <shahidkhan96@gmail.com>
2021-01-03 19:32:00 +0530

    serviceWorker

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Checkout.css";
+import Subtotal from "./Subtotal"
 function Checkout() {
     return (
         <div className="checkout">
@@ -8,10 +9,16 @@ function Checkout() {
 
                 <div>
                     <h2 className="checkout__title">Your Shopping Basket</h2>
+                    {/* BasketItem */}
+                    {/* BasketItem */} 
+                    {/* BasketItem */}
+                    {/* BasketItem */}
+
                 </div>
             </div>
             <div className="checkout__right">
-                
+                {/* Subtotal */}
+                <Subtotal/>
 
             </div>
         </div>
--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,13 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className='header'>
-            <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            <Link to="/">
+                <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            </Link>
         
         <div className="header__search">
             <input className="header__searchInput" type="text"/><SearchIcon className="header__searchIcon"/>
@@ -28,12 +31,13 @@ function Header() {
                 <span className="header__optionLineTwo">Prime</span>
                 
             </div>
-
+            <Link to="/checkout">
             <div className="header__OptionBasket">
-                <ShoppingCartIcon/>
+               <ShoppingCartIcon/>
                 <span className="header__optionLineTwo header__basketCount">0</span>
 
             </div>
+            </Link>
 
         </div>
             </div>
